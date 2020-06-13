cd7414654eeb1ccaf0f56d7e0298d5a852bcfa92
Sergej Kolpakov <sergeikalpakov@gmail.com>
2020-06-13 21:12:30 +0300

    pug & scss & nuxt-i18n

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,88 @@
-
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
+    [
+      "nuxt-i18n",
+      {
+        /* module options */
+      }
+    ]
   ],
+  i18n: {
+    locales: ["en", "lv", "ru"],
+    defaultLocale: "lv",
+    vueI18n: {
+      fallbackLocale: "lv",
+      messages: {
+        en: {
+          welcome: "Welcome!",
+          pages: {
+            contacts: {
+              title: "Want to contact us?"
+            }
+          }
+        },
+        lv: {
+          welcome: "Labdien!",
+          pages: {
+            contacts: {
+              title: "Vēlaties sazināties?"
+            }
+          }
+        },
+        ru: {
+          welcome: "Приветик!",
+          pages: {
+            contacts: {
+              title: "Хотите связаться с нами?"
+            }
+          }
+        }
+      }
+    }
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
---
+++ pages/contacts.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <nuxt-link :to="localePath('/')">home</nuxt-link>
+    <h1>{{ $t("pages.contacts.title") }}</h1>
+  </div>
+</template>
+
